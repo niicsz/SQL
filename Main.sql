0aa25e9ca3d5a6fdb520f9e5829966af8da4c6ad
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-05-07 21:39:00 -0300

    Create Main.sql

---
+++ Main.sql
@@ -0,0 +1,17 @@
+create database if not exists universidade;
+use universidade;
+create table if not exists aluno(
+id_aluno int auto_increment primary key,
+nome_aluno varchar(100) not null,
+telefone bigint not null,
+altura decimal(3,2)
+) ;
+
+create table if not exists professor(
+id_profesor int auto_increment primary key,
+nome_professor varchar(100) not null,
+telefone bigint not null,
+altura decimal(3,2),
+id_aluno int,
+constraint fk_AlunoProfessor foreign key (id_aluno) references aluno (id_aluno)
+) ;
